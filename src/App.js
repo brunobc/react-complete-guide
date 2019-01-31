a2a970a9ae8f4159b148bf350b0d6d89e33cf830
brunobc <brunobezerrachaves@gmail.com>
2019-01-30 23:54:58 -0300

    Handling events onClick

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Jose', age: 45},
+      { name: 'Joao', age: 30},
+      { name: 'Maria', age: 25}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('clicked');
+  }
+
   render() {
     return (
       <div className="App">
@@ -22,9 +34,11 @@ class App extends Component {
           </a>
         </header>
 
-        <Person name="Jose" age="45"/>
-        <Person name="Joao" age="30">My Hobbies: Racing</Person>
-        <Person name="Maria" age="25"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
